b4244cdcc822c8d9e963178450b05e1e095f3c8c
Drew Ayling <drew.ayling@livongo.com>
2022-09-21 15:08:19 -0600

    remove sha

--- Formula/outlook-sound-sync.rb
+++ Formula/outlook-sound-sync.rb
@@ -2,7 +2,6 @@ class OutlookSoundSync < Formula
   desc "Replace default outlook sounds on mac via launchd"
   homepage "https://github.com/dsayling/outlook-sound-sync"
   url "https://github.com/dsayling/outlook-sound-sync/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "2bd91a45a5c30e6342d64710d8f8fb7fef0842ae69f90f689fd823608b0e0708"
   license :public_domain
 
 
