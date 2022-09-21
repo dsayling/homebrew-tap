b2aa68a4f9f204218a6250cb7e1516b17d6ff929
Drew Ayling <drew.ayling@livongo.com>
2022-09-21 15:25:34 -0600

    update to v0.0.5

--- Formula/outlook-sound-sync.rb
+++ Formula/outlook-sound-sync.rb
@@ -1,7 +1,7 @@
 class OutlookSoundSync < Formula
   desc "Replace default outlook sounds on mac via launchd"
   homepage "https://github.com/dsayling/outlook-sound-sync"
-  url "https://github.com/dsayling/outlook-sound-sync/archive/refs/tags/v0.0.4.tar.gz"
+  url "https://github.com/dsayling/outlook-sound-sync/archive/refs/tags/v0.0.5.tar.gz"
   license :public_domain
 
 
